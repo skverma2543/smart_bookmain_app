87567f5fecf8b3e1a6ac2acd2f0e4b929e721ccd
skverma2543 <skverma2543@gmail.com>
2026-02-15 11:47:50 +0530

    glowing background

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { supabase } from "@/lib/supabaseClient";
 import { useRouter } from "next/navigation";
 import { motion } from "framer-motion";
+import { div } from "framer-motion/m";
 
 export default function Dashboard() {
   const router = useRouter();
@@ -52,11 +53,15 @@ export default function Dashboard() {
 
   return (
     <motion.div
-      className="min-h-screen flex items-center justify-center p-6"
+      className="relative min-h-screen flex items-center justify-center p-6 overflow-hidden"
+      
       initial={{ opacity: 0, y: 30 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.4 }}
     >
+       <div className="absolute w-72 h-72 bg-blue-500/30 rounded-full blur-3xl top-10 left-10"></div>
+  <div className="absolute w-72 h-72 bg-purple-500/30 rounded-full blur-3xl bottom-10 right-10"></div>
+
       <div className="w-full max-w-2xl backdrop-blur-md bg-white/10 border border-white/20 rounded-xl p-6 shadow-xl">
 
         <div className="flex justify-between items-center mb-4">
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -16,11 +16,14 @@ export default function LoginPage() {
 
   return (
     <motion.div
-      className="min-h-screen flex items-center justify-center p-6"
+      className="relative min-h-screen flex items-center justify-center p-6 overflow-hidden"
       initial={{ opacity: 0, y: 30 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.4 }}
     >
+       <div className="absolute w-72 h-72 bg-blue-500/30 rounded-full blur-3xl top-10 left-10"></div>
+  <div className="absolute w-72 h-72 bg-purple-500/30 rounded-full blur-3xl bottom-10 right-10"></div>
+
       <div className="backdrop-blur-md bg-white/10 border border-white/20 rounded-xl p-8 shadow-xl text-center max-w-md w-full">
 
         <h1 className="text-3xl font-bold text-white mb-2">
